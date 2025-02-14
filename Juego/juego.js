3097647d81148d3e6799c31358b6f140adf6af8a
Angelo <angelo.iannelli@correo.unimet.edu.ve>
2025-02-13 20:26:37 -0400

    implementacion de funciones de sonido y play en el js

--- Juego/juego.js
+++ Juego/juego.js
@@ -1,41 +1,86 @@
 let successCount = 0;
-let order = [];
+let secuenceOrder = [];
 let playerOrder = [];
 let flash;
 let turn;
 let good;
 let compTurn;
-let intervalId;
+let interval;
 let noise = true;
 let on = false;
 
-const topLeft = document.querySelector("#topleft");
-const bottomLeft = document.querySelector("#bottomleft");
-const topRight = document.querySelector("#topright");
-const bottomRight = document.querySelector("#bottomright");
+const topLeft = document.querySelector("#button-top-left");
+const bottomLeft = document.querySelector("#button-bottom-left");
+const topRight = document.querySelector("#button-top-right");
+const bottomRight = document.querySelector("#button-bottom-right");
 
-//function play() {
-//  flash = 0;
-//  intervalId = 0;
-//  turn = 1;
-//  turnCounter.innerHTML = 1;
-//  good = true;
-//  for (var i = 0; i < 20; i++) {
-//    order.push(Math.floor(Math.random() * 4) + 1);
-//  }
-//  compTurn = true;
-//
-//  intervalId = setInterval(gameTurn, 800);
-//}
+function play() {
+  flash = 0;
+  interval = 0;
+  document.querySelector("#puntaje-acum").innerHTML = 1;
+  good = true;
+  for (var i = 0; i < 20; i++) {
+    let random_color = Math.floor(Math.random() * 4) + 1
+    secuenceOrder.push(random_color);
+  }
+  compTurn = true;
 
-//Repetir para los demás botones
-function button_one_color() {
+  interval = setInterval(gameTurn, 800);
+}
+
+function gameTurn() {
+  on = false;
+
+  if (flash == turn) {
+    clearInterval(interval);
+    compTurn = false;
+    clearColor();
+    on = true;
+  }
+
+  if (compTurn) {
+    clearColor();
+    setTimeout(() => {
+      if (order[flash] == 1) top_left_effects();
+      if (order[flash] == 2) bottom_left_effects();
+      if (order[flash] == 3) top_right_effects();
+      if (order[flash] == 4) bottom_right_effects();
+      flash++;
+    }, 200);
+  }
+}
+
+function top_left_effects() {
   if (noise) {
-    let audio = document.getElementById("sound1");
+    let audio = document.querySelector("sound1");
     audio.play();
   }
   noise = true;
-  //topLeft.style.backgroundColor = "lightgreen";
+  topLeft.style.backgroundColor = "lightblue";
+}
+function bottom_left_effects() {
+  if (noise) {
+    let audio = document.querySelector("sound2");
+    audio.play();
+  }
+  noise = true;
+  bottomLeft.style.backgroundColor = "lightyellow";
+}
+function top_right_effects() {
+  if (noise) {
+    let audio = document.querySelector("sound3");
+    audio.play();
+  }
+  noise = true;
+  topRight.style.backgroundColor = "lightred";
+}
+function bottom_right_effects() {
+  if (noise) {
+    let audio = document.querySelector("sound4");
+    audio.play();
+  }
+  noise = true;
+  bottomRight.style.backgroundColor = "lightgreen";
 }
 
 
